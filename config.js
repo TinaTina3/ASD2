5d0f966891db9205826fc1c79b9da624e12172d0
TinaTina3 <99197255+TinaTina3@users.noreply.github.com>
2025-07-10 20:49:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -24,20 +24,20 @@ window.cfg = {
     {
       title: "Question 3",
       options: [
-        { image: "images\a1.jpg", comment: "Retour sur A1.", value: true },
-        { image: "images\a2.jpg", comment: "Retour sur A2.", value: false },
-        { image: "images\a3.jpg", comment: "Retour sur A3.", value: 3 },
-        { image: "images\a4.jpg", comment: "Retour sur A4.", value: 4 },
-        { image: "images\a1.jpg", comment: "Retour sur A5.", value: 5 }
+        { image: "images/a1.jpg", comment: "Retour sur A1.", value: true },
+        { image: "images/a2.jpg", comment: "Retour sur A2.", value: false },
+        { image: "images/a3.jpg", comment: "Retour sur A3.", value: 3 },
+        { image: "images/a4.jpg", comment: "Retour sur A4.", value: 4 },
+        { image: "images/a1.jpg", comment: "Retour sur A5.", value: 5 }
       ]
     },
     {
       title: "Question 4",
       options: [
-        { image: "images\f13_dfh_fc.jpg", comment: "Great thats totally it", value: 1 },
-        { image: "images\f13_dfh_ac.jpg", comment: "No good !", value: 2 },
-        { image: "images\f13_dfh_crc.jpg", comment: "Almost there.No good !", value: 3 },
-        { image: "images\f13_dfh_dc.jpg", comment: "Very bad", value: 4 }
+        { image: "images/f13_dfh_fc.jpg", comment: "Great thats totally it", value: 1 },
+        { image: "images/f13_dfh_ac.jpg", comment: "No good !", value: 2 },
+        { image: "images/f13_dfh_crc.jpg", comment: "Almost there.No good !", value: 3 },
+        { image: "images/f13_dfh_dc.jpg", comment: "Very bad", value: 4 }
       ]
     }
     // Ajoutez d'autres questions ici…
